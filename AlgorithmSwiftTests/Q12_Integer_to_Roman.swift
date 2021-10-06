8a25f64c433ec242c4322fedcad6dd2b2ba11cf6
onebroccoli <three.broccoli@gmail.com>
2021-10-06 14:52:36 -0700

    Create Q12_Integer_to_Roman.swift

---
+++ AlgorithmSwiftTests/Q12_Integer_to_Roman.swift
@@ -0,0 +1,82 @@
+//
+//  Q12_Integer_to_Roman.swift
+//  AlgorithmSwiftTests
+//
+//  Created by Sophia Zhu on 8/31/21.
+//
+/*
+ Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+ Symbol       Value
+ I             1
+ V             5
+ X             10
+ L             50
+ C             100
+ D             500
+ M             1000
+ For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+ Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+ I can be placed before V (5) and X (10) to make 4 and 9.
+ X can be placed before L (50) and C (100) to make 40 and 90.
+ C can be placed before D (500) and M (1000) to make 400 and 900.
+ Given an integer, convert it to a roman numeral.
+
+  
+
+ Example 1:
+
+ Input: num = 3
+ Output: "III"
+ Example 2:
+
+ Input: num = 4
+ Output: "IV"
+ Example 3:
+
+ Input: num = 9
+ Output: "IX"
+ Example 4:
+
+ Input: num = 58
+ Output: "LVIII"
+ Explanation: L = 50, V = 5, III = 3.
+ Example 5:
+
+ Input: num = 1994
+ Output: "MCMXCIV"
+ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+  
+
+ Constraints:
+
+ 1 <= num <= 3999
+ 
+ */
+import XCTest
+
+class Q12_Integer_to_Roman: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
