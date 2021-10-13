3b0c96327b1fa0bbf51aa7e745ace3cd16f92cd0
onebroccoli <three.broccoli@gmail.com>
2021-10-12 17:06:55 -0700

    Update Q46_Permutations.swift

--- AlgorithmSwiftTests/Q46_Permutations.swift
+++ AlgorithmSwiftTests/Q46_Permutations.swift
@@ -32,45 +32,45 @@
 import XCTest
 
 private class Solution {
-    func permute(_ nums: [Int]) -> [[Int]]{
-        var result = [[Int]]()
-        //sanity check
-        if nums.count == 0 {
-            return result
-        }
-        //choose the character to be at the position of "Index"
-          //all the already chosen positions are (0, index - 1)
-          //all the candidate characters can be at position "index"
-          //are in the subarray of (index, array.length-1)
-        var array = nums
-        dfs(&array, 0, &result)
+func permute(_ nums: [Int]) -> [[Int]]{
+    var result = [[Int]]()
+    //sanity check
+    if nums.count == 0 {
         return result
     }
-    func dfs(_ nums: inout [Int], _ index: Int, _ result: inout [[Int]]){
-        //terminate condition:
-        //only when we have already chosen the characters for all the positions,
-        //we can have a complete permutation
-        if index == nums.count - 1 {
-            result.append(nums)
-            return
-        }
-        //all the possible numbers could be placed at index are
-        //the numbers in the subarray (index, nums.length-1)
-        for i in index..<nums.count {
-            swap(&nums, index, i)
-            dfs(&nums, index + 1, &result)
-            //remember to swap back when back track to previou level
-            swap(&nums, index, i)
+    //choose the character to be at the position of "Index"
+      //all the already chosen positions are (0, index - 1)
+      //all the candidate characters can be at position "index"
+      //are in the subarray of (index, array.length-1)
+    var array = nums
+    dfs(&array, 0, &result)
+    return result
+}
+func dfs(_ nums: inout [Int], _ index: Int, _ result: inout [[Int]]){
+    //terminate condition:
+    //only when we have already chosen the characters for all the positions,
+    //we can have a complete permutation
+    if index == nums.count - 1 {
+        result.append(nums)
+        return
+    }
+    //all the possible numbers could be placed at index are
+    //the numbers in the subarray (index, nums.length-1)
+    for i in index..<nums.count {
+        swap(&nums, index, i)
+        dfs(&nums, index + 1, &result)
+        //remember to swap back when back track to previou level
+        swap(&nums, index, i)
 
-        }
     }
-    
-    //swap function
-    func swap(_ nums: inout [Int], _ left: Int, _ right: Int) {
-        let tmp = nums[left]
-        nums[left] = nums[right]
-        nums[right] = tmp
-    }
+}
+
+//swap function
+func swap(_ nums: inout [Int], _ left: Int, _ right: Int) {
+    let tmp = nums[left]
+    nums[left] = nums[right]
+    nums[right] = tmp
+}
 }
 
 
