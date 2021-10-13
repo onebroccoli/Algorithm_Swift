25ae57ba983ddbaad564ceb9d0ec8667880e3aaf
onebroccoli <three.broccoli@gmail.com>
2021-10-12 17:07:03 -0700

    Update Q90_Subsets_II.swift

--- AlgorithmSwiftTests/Q90_Subsets_II.swift
+++ AlgorithmSwiftTests/Q90_Subsets_II.swift
@@ -23,35 +23,35 @@
 
 import XCTest
 private class Solution {
-    func subsetsWithDup(_ nums: [Int]) -> [[Int]] {
-        var res = [[Int]]()
-        var path = [Int]()
-        //make sure the multi-set is sorted so that we can deduplicate.
-        let nums = nums.sorted(by: <)
-        helper(&res, &path, nums, 0)
+func subsetsWithDup(_ nums: [Int]) -> [[Int]] {
+    var res = [[Int]]()
+    var path = [Int]()
+    //make sure the multi-set is sorted so that we can deduplicate.
+    let nums = nums.sorted(by: <)
+    helper(&res, &path, nums, 0)
 
-        return res
+    return res
+}
+//index: at current level, determine if the element at "index" should be included in the subset or not
+private func helper(_ res: inout [[Int]],  _ path: inout [Int], _ nums: [Int], _ index:  Int) {
+    var index = index
+    // termination case
+    if index == nums.count {
+        res.append(path)
+        return
     }
-    //index: at current level, determine if the element at "index" should be included in the subset or not
-    private func helper(_ res: inout [[Int]],  _ path: inout [Int], _ nums: [Int], _ index:  Int) {
-        var index = index
-        // termination case
-        if index == nums.count {
-            res.append(path)
-            return
-        }
-        //case1: add
-        path.append(nums[index])
-        helper(&res, &path, nums, index + 1)
-        path.removeLast()
+    //case1: add
+    path.append(nums[index])
+    helper(&res, &path, nums, index + 1)
+    path.removeLast()
 
-        //case2: //case2: not add when duplicates
-        //skip all the consecutive and duplicate elements
-        while index < nums.count - 1 && nums[index + 1] == nums[index] {
-            index += 1
-        }
-        helper(&res, &path, nums, index + 1)
+    //case2: //case2: not add when duplicates
+    //skip all the consecutive and duplicate elements
+    while index < nums.count - 1 && nums[index + 1] == nums[index] {
+        index += 1
     }
+    helper(&res, &path, nums, index + 1)
+}
 
 }
 
