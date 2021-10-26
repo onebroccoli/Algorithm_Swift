fd4606a0907b7da34e5dec2964354d473a796274
onebroccoli <three.broccoli@gmail.com>
2021-10-26 16:49:11 -0700

    Update Q203_Remove_Linked_List_Elements.swift

--- AlgorithmSwiftTests/Q203_Remove_Linked_List_Elements.swift
+++ AlgorithmSwiftTests/Q203_Remove_Linked_List_Elements.swift
@@ -32,49 +32,38 @@
  */
 import XCTest
 private class Solution {
-func removeElements(_ head: ListNode?, _ val: Int) -> ListNode? {
-    var head = head
-    if head == nil {
+    func removeElements(_ head: ListNode?, _ val: Int) -> ListNode? {
+        var head = head
+        if head == nil {
+            return head
+        }
+        var temp = head?.next
+        var prev = head
+        while temp != nil {
+            if temp?.val == val {
+                prev?.next = temp?.next
+                temp = prev?.next
+            } else {
+                temp = temp?.next
+                prev = prev?.next
+            }
+        }
+        //post processing: head == val
+        if head?.val == val {
+            head = head?.next
+        }
         return head
     }
-    var temp = head?.next
-    var prev = head
-    while temp != nil {
-        if temp?.val == val {
-            prev?.next = temp?.next
-            temp = prev?.next
-        } else {
-            temp = temp?.next
-            prev = prev?.next
-        }
-    }
-    if head?.val == val {
-        head = head?.next
-    }
-    return head
-}
 }
 
 class Q203_Remove_Linked_List_Elements: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let s = Solution()
+        let arr = [1,2,3,4]
+        let list = LinkedList(arr)
+        let result = s.removeElements(list.head, 3)
+        
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
