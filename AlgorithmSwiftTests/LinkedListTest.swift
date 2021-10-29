e3e12da2ad1bcefcba4460af660c55ebc637af01
onebroccoli <three.broccoli@gmail.com>
2021-10-28 23:28:11 -0700

    Create LinkedListTest.swift

---
+++ AlgorithmSwiftTests/LinkedListTest.swift
@@ -0,0 +1,16 @@
+//
+//  LinkedListTest.swift
+//  AlgorithmSwiftTests
+//
+//  Created by Sophia Zhu on 10/28/21.
+//
+
+import XCTest
+
+class LinkedListTest: XCTestCase {
+    func testExample() throws {
+        let a = LinkedList([1,2,3,4])
+        print(a)
+        print("++++++++DONE!!!++++++++")
+    }
+}
