70454dc352c89e459f358342dc779869573d5da1
onebroccoli <three.broccoli@gmail.com>
2021-11-24 15:38:22 -0800

    Create Q344_Reverse_String.swift

---
+++ AlgorithmSwiftTests/Q344_Reverse_String.swift
@@ -0,0 +1,38 @@
+//
+//  Q344_Reverse_String.swift
+//  AlgorithmSwiftTests
+//
+//  Created by Sophia Zhu on 11/24/21.
+//
+
+/*
+ (396). Reverse String
+
+ Write a function that reverses a string. The input string is given as an array of characters s.
+ You must do this by modifying the input array in-place with O(1) extra memory.
+  
+ Example 1:
+ Input: s = ["h","e","l","l","o"]
+ Output: ["o","l","l","e","h"]
+
+ Example 2:
+ Input: s = ["H","a","n","n","a","h"]
+ Output: ["h","a","n","n","a","H"]
+
+
+
+ */
+import XCTest
+
+class Q344_Reverse_String: XCTestCase {
+
+   
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+   
+
+}
