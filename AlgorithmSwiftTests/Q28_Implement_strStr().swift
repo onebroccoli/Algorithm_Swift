f1ddeb6634e78afbf18aa43db7fe459ce2e971f7
onebroccoli <three.broccoli@gmail.com>
2022-01-05 23:55:44 -0800

    Update Q28_Implement_strStr().swift

--- AlgorithmSwiftTests/Q28_Implement_strStr().swift
+++ AlgorithmSwiftTests/Q28_Implement_strStr().swift
@@ -33,47 +33,47 @@
 import XCTest
 
 private class Solution {
-func strStr(_ haystack: String, _ needle: String) -> Int {
-    let count1 = haystack.count
-    let count2 = needle.count
-    if count2 == 0 {
-        return 0
-    }
-    
-    if count1 < count2 {
+    func strStr(_ haystack: String, _ needle: String) -> Int {
+        let count1 = haystack.count
+        let count2 = needle.count
+        if count2 == 0 {
+            return 0
+        }
+        
+        if count1 < count2 {
+            return -1
+        }
+        
+        var haystackChars = haystack.cString(using: .utf8)!
+        var needleChars = needle.cString(using: .utf8)!
+        var i = 0
+        var j = 0
+        
+        let maxi = count1 - count2
+        let maxj = count2 - 1
+        
+        while i <= maxi && j <= maxj {
+            var m = i
+            while m <= count1 - 1 && j <= maxj {
+                let mv = haystackChars[m]
+                let jv = needleChars[j]
+                if mv == jv {
+                    m += 1
+                    j += 1
+                    continue
+                }
+                j = 0
+                i += 1
+                break
+            }
+        }
+        j = j - 1
+        if j == maxj{
+            return i
+        }
+        
         return -1
     }
-    
-    var haystackChars = haystack.cString(using: .utf8)!
-    var needleChars = needle.cString(using: .utf8)!
-    var i = 0
-    var j = 0
-    
-    let maxi = count1 - count2
-    let maxj = count2 - 1
-    
-    while i <= maxi && j <= maxj {
-        var m = i
-        while m <= count1 - 1 && j <= maxj {
-            let mv = haystackChars[m]
-            let jv = needleChars[j]
-            if mv == jv {
-                m += 1
-                j += 1
-                continue
-            }
-            j = 0
-            i += 1
-            break
-        }
-    }
-    j = j - 1
-    if j == maxj{
-        return i
-    }
-    
-    return -1
-}
 }
 
 
