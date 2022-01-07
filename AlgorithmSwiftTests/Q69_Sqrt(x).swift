bfe4b5386b4efde5a77ba7a22a1d5cf1f95ac08a
onebroccoli <three.broccoli@gmail.com>
2022-01-06 22:15:40 -0800

    Update Q69_Sqrt(x).swift

--- AlgorithmSwiftTests/Q69_Sqrt(x).swift
+++ AlgorithmSwiftTests/Q69_Sqrt(x).swift
@@ -25,24 +25,24 @@
  */
 import XCTest
 private class Solution {
-    func mySqrt(_ x: Int) -> Int {
-        if x < 2 {
-            return x
+func mySqrt(_ x: Int) -> Int {
+    if x < 2 {
+        return x
+    }
+    var left = 2
+    var right = x / 2
+    while left <= right {
+        var mid = left + (right - left) / 2
+        var num = mid * mid
+        if num > x {
+            right = mid - 1
+        } else if num < x {
+            left = mid + 1
+        } else if num == x {
+            return mid
         }
-        var left = 2
-        var right = x / 2
-        while left <= right {
-            var mid = left + (right - left) / 2
-            var num = mid * mid
-            if num > x {
-                right = mid - 1
-            } else if num < x {
-                left = mid + 1
-            } else if num == x {
-                return mid
-            }
-        }
-        return right
+    }
+    return right
             
         
     }
