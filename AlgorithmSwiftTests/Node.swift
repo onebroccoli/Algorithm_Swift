30377ba9716ff13828377809fe776d15acabfccb
onebroccoli <three.broccoli@gmail.com>
2022-01-14 00:41:46 -0800

    Create Node.swift

---
+++ AlgorithmSwiftTests/Node.swift
@@ -0,0 +1,17 @@
+//
+//  Node.swift
+//  AlgorithmSwiftTests
+//
+//  Created by Sophia Zhu on 1/14/22.
+//
+
+import Foundation
+
+public class Node {
+    public var val: Int
+    public var neighbors: [Node?]
+    public init(_ val: Int){
+        self.val = val
+        self.neighbors = []
+    }
+}
