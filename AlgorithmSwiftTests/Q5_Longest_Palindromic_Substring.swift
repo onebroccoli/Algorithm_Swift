eec87c2e68a49854d33085c07d14311f00f18d02
onebroccoli <three.broccoli@gmail.com>
2022-01-19 15:37:29 -0800

    Create Q5_Longest_Palindromic_Substring.swift

---
+++ AlgorithmSwiftTests/Q5_Longest_Palindromic_Substring.swift
@@ -0,0 +1,56 @@
+//
+//  Q5_Longest_Palindromic_Substring.swift
+//  AlgorithmSwiftTests
+//
+//  Created by Sophia Zhu on 1/18/22.
+//
+/*
+ 5. Longest Palindromic Substring
+ Given a string s, return the longest palindromic substring in s.
+
+  
+
+ Example 1:
+
+ Input: s = "babad"
+ Output: "bab"
+ Explanation: "aba" is also a valid answer.
+ Example 2:
+
+ Input: s = "cbbd"
+ Output: "bb"
+  
+
+
+ */
+import XCTest
+private class Solution {
+    
+    
+}
+class Q5_Longest_Palindromic_Substring: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
