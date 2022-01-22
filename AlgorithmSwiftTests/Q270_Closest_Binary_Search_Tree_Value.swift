cd51168bd7b10c351690e3a356d2a8e9ea240eb5
onebroccoli <three.broccoli@gmail.com>
2022-01-21 23:46:10 -0800

    Create Q270_Closest_Binary_Search_Tree_Value.swift

---
+++ AlgorithmSwiftTests/Q270_Closest_Binary_Search_Tree_Value.swift
@@ -0,0 +1,52 @@
+//
+//  Q270_Closest_Binary_Search_Tree_Value.swift
+//  AlgorithmSwiftTests
+//
+//  Created by Sophia Zhu on 1/21/22.
+//
+/*
+ 270. Closest Binary Search Tree Value
+ Given the root of a binary search tree and a target value, return the value in the BST that is closest to the target.
+
+  
+
+ Example 1:
+
+
+ Input: root = [4,2,5,1,3], target = 3.714286
+ Output: 4
+ Example 2:
+
+ Input: root = [1], target = 4.428571
+ Output: 1
+ */
+import XCTest
+private class Solution {
+    
+}
+class Q270_Closest_Binary_Search_Tree_Value: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
