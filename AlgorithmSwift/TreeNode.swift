bf54b557640a40b16ce0beeee7d6ece8b435bf11
onebroccoli <three.broccoli@gmail.com>
2022-02-12 22:54:49 -0800

    Update TreeNode.swift

--- AlgorithmSwift/TreeNode.swift
+++ AlgorithmSwift/TreeNode.swift
@@ -14,7 +14,7 @@ public class TreeNode {
     public var val: Int
     public var left: TreeNode?
     public var right: TreeNode?
-    public var next : TreeNode?
+    public var next: TreeNode?
     public init() { self.val = 0; self.left = nil; self.right = nil; self.next = nil }
     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; self.next = nil}
     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?, _ next: TreeNode?) {
