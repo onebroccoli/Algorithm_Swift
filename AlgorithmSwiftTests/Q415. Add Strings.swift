8a714d89361625fb34b6779b2c905daabcba776b
onebroccoli <three.broccoli@gmail.com>
2022-02-15 12:51:08 -0800

    Update Q415. Add Strings.swift

--- AlgorithmSwiftTests/Q415. Add Strings.swift
+++ AlgorithmSwiftTests/Q415. Add Strings.swift
@@ -55,30 +55,97 @@ private class Solution {
         return String(res.reversed())
     }
     
+    //变种 如果带小数点
+    //32.17
+    //3.192
+    
+    //  2 3
+    //+ 3
+    // 5 3  reverse 3 5
+    
+    // 0 7 1
+    //+2 9 1
+    // 2 6 3 reverse 3 6 2
+    
+    
+    
+    func addInteger_withdigit(_ num1: String, _ num2: String) -> String {
+        //separate integer and decimal parts
+        let number1 = num1.components(separatedBy: ".")
+        let ch1:[Character] = Array(number1[0]).reversed()
+        var dec1: [Character] = number1.count == 1 ? ["0"] : Array(number1[1]).reversed()
+        
+        let number2 = num2.components(separatedBy: ".")
+        let ch2:[Character] = Array(number2[0]).reversed()
+        var dec2: [Character] = number2.count == 1 ? ["0"] : Array(number2[1]).reversed()
+ 
+        
+       
+        
+        
+        
+        //process two decimal variables make sure they are same length by adding 0
+        
+        if dec1.count < dec2.count {
+            var i = dec2.count - dec1.count
+            while i > 0 {
+                dec1.insert("0", at: 0)
+                i -= 1
+            }
+        } else if dec1.count > dec2.count {
+            var i = dec1.count - dec2.count
+            while i > 0 {
+                dec2.insert("0", at: 0)
+                i -= 1
+            }
+        }
+        
+        //adding two decimal first
+        var carry = 0
+        var resDecimal = [Character]()
+        var j = dec1.count
+        if j > 0 {
+            for i in 0..<j {
+                var int1 = Int(String(dec1[i]))!
+                var int2 = Int(String(dec2[i]))!
+                var value = (int1 + int2 + carry) % 10
+                carry = (int1 + int2 + carry) / 10
+                resDecimal.append(Character(String(value)))
+            }
+        }
+
+        
+        //adding integer
+        var resInteger = [Character]()
+        var l1 = 0
+        var l2 = 0
+        while l1 < ch1.count || l2 < ch2.count || carry == 1 {
+            let char1 = l1 < ch1.count ? Int(String(ch1[l1]))! : 0
+            let char2 = l2 < ch2.count ? Int(String(ch2[l2]))! : 0
+            var value = (char1 + char2 + carry) % 10
+            carry = (char1 + char2 + carry) / 10
+            l1 += 1
+            l2 += 1
+            resInteger.append(Character(String(value)))
+        }
+    
+        return String(resInteger.reversed() + ["."] + resDecimal.reversed())
+        
+
+    }
+    
 }
 class Q415__Add_Strings: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+   
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        let s = Solution()
+        let a: String = "32.17"
+        let b : String = "3.192"
+        let result = s.addInteger_withdigit(a, b)
+        print("result ======: ", result)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+  
 
 }
