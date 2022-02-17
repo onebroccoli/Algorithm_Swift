7b6f7f4259cd05c1b6ae6f99c2e112fb524ca54b
onebroccoli <three.broccoli@gmail.com>
2022-02-16 16:51:31 -0800

    Update Q14_Longest_Common_Prefix.swift

--- AlgorithmSwiftTests/Q14_Longest_Common_Prefix.swift
+++ AlgorithmSwiftTests/Q14_Longest_Common_Prefix.swift
@@ -33,6 +33,33 @@ func findCommonPrefix(_ str1: String, _ str2: String) -> String {
     }
     return output
 }
+    
+    
+func longestCommonPrefix2(_ strs: [String]) -> String {
+    if strs == nil || strs.count == 0 {
+        return ""
+    }
+    var output = ""
+    var char_str = Array(strs[0])
+    var numberCompare = strs.count - 1
+    for i in 0..<char_str.count {
+        var counter = 0
+        for j in 1..<strs.count {
+            var char_compareStr = Array(strs[j])
+            if i < char_compareStr.count {
+                if char_str[i] != char_compareStr[i] {
+                    return output
+                } else {
+                    counter += 1
+                }
+            }
+        }
+        if counter == numberCompare {
+            output += String(char_str[i])
+        }
+    }
+    return output
+}
 }
 
 
