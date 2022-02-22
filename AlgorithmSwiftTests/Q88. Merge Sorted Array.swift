e99e295bd28874270acd4e47d8342c2c8929daab
onebroccoli <three.broccoli@gmail.com>
2022-02-21 23:53:44 -0800

    Create Q88. Merge Sorted Array.swift

---
+++ AlgorithmSwiftTests/Q88. Merge Sorted Array.swift
@@ -0,0 +1,78 @@
+//
+//  Q88. Merge Sorted Array.swift
+//  AlgorithmSwiftTests
+//
+//  Created by Sophia Zhu on 2/17/22.
+//
+/*
+ 88. Merge Sorted Array
+ You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+ Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+ The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+  
+
+ Example 1:
+
+ Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ Output: [1,2,2,3,5,6]
+ Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+ The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+ Example 2:
+
+ Input: nums1 = [1], m = 1, nums2 = [], n = 0
+ Output: [1]
+ Explanation: The arrays we are merging are [1] and [].
+ The result of the merge is [1].
+ Example 3:
+
+ Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+ Output: [1]
+ Explanation: The arrays we are merging are [] and [1].
+ The result of the merge is [1].
+ Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+  
+
+ Constraints:
+
+ nums1.length == m + n
+ nums2.length == n
+ 0 <= m, n <= 200
+ 1 <= m + n <= 200
+ -109 <= nums1[i], nums2[j] <= 109
+
+ */
+import XCTest
+private class Solution {
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        var k = m + n - 1
+        var i = m - 1
+        var j = n - 1
+        while k >= 0 && j >= 0 {
+            if i > 0 && nums1[i] > nums2[j] {
+                nums1[k] = nums1[i]
+                i -= 1
+            } else {
+                nums1[k] = nums2[j]
+                j -= 1
+            }
+            k -= 1
+        }
+    }
+}
+class Q88__Merge_Sorted_Array: XCTestCase {
+
+   
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+ 
+
+}
