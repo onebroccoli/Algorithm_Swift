065dec39dddf2b6b7b4df6fe4d9270a36410d761
onebroccoli <three.broccoli@gmail.com>
2022-03-01 00:00:24 -0800

    Update Q560. Subarray Sum Equals K.swift

--- AlgorithmSwiftTests/Q560. Subarray Sum Equals K.swift
+++ AlgorithmSwiftTests/Q560. Subarray Sum Equals K.swift
@@ -27,30 +27,34 @@
  -107 <= k <= 107
  */
 import XCTest
-
+private class Solution {
+func subarraySum(_ nums: [Int], _ k: Int) -> Int {
+    var count = 0
+    var sum = 0
+    var map =  [Int : Int]()
+    map[0] = 1
+    for i in 0..<nums.count {
+        sum += nums[i]
+        if map.keys.contains(sum - k) {
+            count += map[sum - k]!
+            
+        }
+        map[sum, default: 0] += 1
+    }
+    return count
+}
+}
 class Q560__Subarray_Sum_Equals_K: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+       let s = Solution()
+        let a = [1, 1, 1]
+        let result = s.subarraySum(a, 2)
+        print("result: === ", result)
+        
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+
 
 }
