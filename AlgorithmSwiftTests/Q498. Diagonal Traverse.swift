1dc86f88a648e9f1e13d15a1e85d99e7660436e9
onebroccoli <three.broccoli@gmail.com>
2022-03-10 12:23:47 -0800

    Update Q498. Diagonal Traverse.swift

--- AlgorithmSwiftTests/Q498. Diagonal Traverse.swift
+++ AlgorithmSwiftTests/Q498. Diagonal Traverse.swift
@@ -31,31 +31,31 @@
  */
 import XCTest
 private class Solution {
-func findDiagonalOrder(_ matrix: [[Int]]) -> [Int] {
-    var result = [Int]()
-    guard matrix.count != 0 else {
+    func findDiagonalOrder(_ matrix: [[Int]]) -> [Int] {
+        var result = [Int]()
+        guard matrix.count != 0 else {
+            return result
+        }
+        let row = matrix.count
+        let col = matrix[0].count
+        var rowIndex = 0
+        var colIndex = 0
+        while result.count != row*col {
+            result.append(matrix[rowIndex][colIndex])
+            if (rowIndex + colIndex) % 2 == 0 {
+                //if last column, rowindex + 1, if not , max(0, rowIndex - 1) go up one level
+                //col: (col - 1, colIndex+1)
+                rowIndex = (colIndex == col - 1) ? rowIndex + 1 : max(0, rowIndex - 1)
+                colIndex = min(col - 1, colIndex + 1)
+            } else {
+                //Col: if last row, col + 1, if not, max(0, colIndex - 1)go left one column
+                //Row: min(row - 1 , rowIndex + 1)
+                colIndex = (rowIndex == row - 1) ? colIndex + 1 : max(0, colIndex - 1)
+                rowIndex = min(row - 1, rowIndex + 1)
+            }
+        }
         return result
     }
-    let row = matrix.count
-    let col = matrix[0].count
-    var rowIndex = 0
-    var colIndex = 0
-    while result.count != row*col {
-        result.append(matrix[rowIndex][colIndex])
-        if (rowIndex + colIndex) % 2 == 0 {
-            //if last column, rowindex + 1, if not , max(0, rowIndex - 1) go up one level
-            //col: (col - 1, colIndex+1)
-            rowIndex = (colIndex == col - 1) ? rowIndex + 1 : max(0, rowIndex - 1)
-            colIndex = min(col - 1, colIndex + 1)
-        } else {
-            //Col: if last row, col + 1, if not, max(0, colIndex - 1)go left one column
-            //Row: min(row - 1 , rowIndex + 1)
-            colIndex = (rowIndex == row - 1) ? colIndex + 1 : max(0, colIndex - 1)
-            rowIndex = min(row - 1, rowIndex + 1)
-        }
-    }
-    return result
-}
 }
 class Q498__Diagonal_Traverse: XCTestCase {
 
