38b4b537aaafef256c2d35f0cc7e054b1066d359
onebroccoli <three.broccoli@gmail.com>
2022-03-16 23:43:44 -0700

    Update Q523. Continuous Subarray Sum.swift

--- AlgorithmSwiftTests/Q523. Continuous Subarray Sum.swift
+++ AlgorithmSwiftTests/Q523. Continuous Subarray Sum.swift
@@ -39,32 +39,6 @@
  */
 import XCTest
 private class Solution {
-//func checkSubarraySum(_ nums: [Int], _ k: Int) -> Bool {
-//    if nums.count == 1 {
-//        return false
-//    }
-//
-//    var prefixSum = nums[0]
-//
-//    var start = 0
-//    var dict = [ nums[0] % k: 0]
-//
-//    for i in 1..<nums.count {
-//        prefixSum += nums[i]
-//        let remainder = prefixSum % k
-//
-//        if remainder == 0 {
-//            return true
-//            //if remainder exists in dictionary, means prefix has the remainder before, and the subarrry is a multiply of k
-//        } else if let secondIndex = dict[remainder] {
-//            if abs(i - secondIndex) >= 2 { return true } //compare the index if the gap is >=2 elements, its true
-//        } else {
-//            dict[remainder] = i
-//        }
-//    }
-//
-//    return false
-//}
     func checkSubarraySum(_ nums: [Int], _ k: Int) -> Bool {
         var dict = [Int: Int]()
         dict[0] = -1
