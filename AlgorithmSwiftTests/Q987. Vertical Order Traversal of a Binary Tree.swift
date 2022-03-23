e95d7d1d2ca0835925d16eedf184d12c30b16146
onebroccoli <three.broccoli@gmail.com>
2022-03-23 16:39:21 -0700

    Update Q987. Vertical Order Traversal of a Binary Tree.swift

--- AlgorithmSwiftTests/Q987. Vertical Order Traversal of a Binary Tree.swift
+++ AlgorithmSwiftTests/Q987. Vertical Order Traversal of a Binary Tree.swift
@@ -57,60 +57,59 @@
  */
 //import XCTest
 private class Solution {
-func verticalTraversal(_ root: TreeNode?) -> [[Int]] {
-    var result: [[Int]] = []
-    var queue: [(node: TreeNode?, column: Int)] = []
-    var hash: [Int: [Int]] = [:] //map, key is row number, value is [Int] storing the elements for each row.
-    
-    queue.append((node: root, column: 0))
-    while !queue.isEmpty {
-        var count = queue.count - 1
-        var temp: [Int: [Int]] = [:]
-        while count >= 0 {
-            let value = queue.removeFirst()
-            if let root = value.node {
-                if let left = root.left {
-                    queue.append((node: left, column: value.column - 1))
-                    
+    func verticalTraversal(_ root: TreeNode?) -> [[Int]] {
+        var result: [[Int]] = []
+        var queue: [(node: TreeNode?, column: Int)] = []
+        var hash: [Int: [Int]] = [:] //map, key is row number, value is [Int] storing the elements for each row.
+        
+        queue.append((node: root, column: 0))
+        while !queue.isEmpty {
+            var count = queue.count - 1
+            var temp: [Int: [Int]] = [:]
+            while count >= 0 {
+                let value = queue.removeFirst()
+                if let root = value.node {
+                    if let left = root.left {
+                        queue.append((node: left, column: value.column - 1))
+                        
+                    }
+                    if let right = root.right {
+                        queue.append((node: right, column: value.column + 1))
+                        
+                    }
+                    //if temp[column] exists, insert root.val
+                    if var isInHash = temp[value.column] {
+                        isInHash.append(root.val)
+                        temp[value.column] = isInHash //update the temp hash value list
+                        //if not exists, append [root.val] directly
+                    } else {
+                        temp[value.column] = [root.val]
+                    }
                 }
-                if let right = root.right {
-                    queue.append((node: right, column: value.column + 1))
-                    
-                }
-                
-                //if temp[column] exists, insert root.val
-                if var isInHash = temp[value.column] {
-                    isInHash.append(root.val)
-                    temp[value.column] = isInHash
-                //if not exists, append [root.val] directly
-                } else {
-                    temp[value.column] = [root.val]
+                count -= 1
+            }
+            
+            if !temp.isEmpty {
+                for (key, value) in temp {
+                    if var isInHash = hash[key] {
+                        isInHash.append(contentsOf: value.sorted())
+                        hash[key] = isInHash
+                    } else {
+                        hash[key] = value.sorted()
+                    }
                 }
             }
-            count -= 1
         }
         
-        if !temp.isEmpty {
-            for (key, value) in temp {
-                if var isInHash = hash[key] {
-                    isInHash.append(contentsOf: value.sorted())
-                    hash[key] = isInHash
-                } else {
-                    hash[key] = value.sorted()
-                }
+        //Formulating the result:
+        for key in hash.keys.sorted() {
+            if let value = hash[key] {
+                result.append(value)
             }
         }
+        return result
+        
     }
-    
-    //Formulating the result:
-    for key in hash.keys.sorted() {
-        if let value = hash[key] {
-            result.append(value)
-        }
-    }
-    return result
-    
-}
    //[3,9,20,null,null,15,7]
     /*
      
