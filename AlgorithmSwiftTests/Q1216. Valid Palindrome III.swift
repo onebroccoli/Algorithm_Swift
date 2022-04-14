6ab287d8d07a200034dd76a8a8944b19b7db072f
onebroccoli <three.broccoli@gmail.com>
2022-04-14 00:03:08 -0700

    Create Q1216. Valid Palindrome III.swift

---
+++ AlgorithmSwiftTests/Q1216. Valid Palindrome III.swift
@@ -0,0 +1,62 @@
+//
+//  Q1216. Valid Palindrome III.swift
+//  AlgorithmSwiftTests
+//
+//  Created by Sophia Zhu on 4/13/22.
+//
+/*
+ 1216. Valid Palindrome III
+
+ Given a string s and an integer k, return true if s is a k-palindrome.
+
+ A string is k-palindrome if it can be transformed into a palindrome by removing at most k characters from it.
+
+  
+
+ Example 1:
+
+ Input: s = "abcdeca", k = 2
+ Output: true
+ Explanation: Remove 'b' and 'e' characters.
+ Example 2:
+
+ Input: s = "abbababa", k = 1
+ Output: true
+  
+
+ Constraints:
+
+ 1 <= s.length <= 1000
+ s consists of only lowercase English letters.
+ 1 <= k <= s.length
+ */
+import XCTest
+private class Solution {
+    
+}
+class Q1216__Valid_Palindrome_III: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
