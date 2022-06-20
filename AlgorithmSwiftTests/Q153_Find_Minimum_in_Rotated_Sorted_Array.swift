3706393b008b26208dff1d20598a9d1f706bb094
onebroccoli <three.broccoli@gmail.com>
2022-06-20 16:58:57 -0700

    Update Q153_Find_Minimum_in_Rotated_Sorted_Array.swift

--- AlgorithmSwiftTests/Q153_Find_Minimum_in_Rotated_Sorted_Array.swift
+++ AlgorithmSwiftTests/Q153_Find_Minimum_in_Rotated_Sorted_Array.swift
@@ -45,28 +45,6 @@
  */
 import XCTest
 private class Solution {
-func findMin(_ nums: [Int]) -> Int {
-    //corner cases
-    if nums == nil || nums.count == 0 {
-        return -1
-    }
-    if nums.count == 1 {
-        return nums[0]
-    }
-    
-    var left = 0
-    var right = nums.count - 1
-    
-    while left < right {
-        var mid = left + (right - left) / 2
-        if nums[mid] <= nums[right] {
-            right = mid
-        } else if nums[mid] > nums[right] {
-            left = mid + 1
-        }
-    }
-    return nums[left]
-}
 //func findMin(_ nums: [Int]) -> Int {
 //    //corner cases
 //    if nums == nil || nums.count == 0 {
@@ -79,28 +57,50 @@ func findMin(_ nums: [Int]) -> Int {
 //    var left = 0
 //    var right = nums.count - 1
 //
-//    if nums[right] > nums[left] {
-//        return nums[left]
-//    }
-//
-//    while left <= right {
+//    while left < right {
 //        var mid = left + (right - left) / 2
-//        //corner case find the rotation point
-//        if nums[mid] > nums[mid + 1] {
-//            return nums[mid + 1]
-//        }
-//        if nums[mid - 1] > nums[mid] {
-//            return nums[mid]
-//        }
-//
-//        if nums[mid] > nums[left] {
+//        if nums[mid] <= nums[right] {
+//            right = mid
+//        } else if nums[mid] > nums[right] {
 //            left = mid + 1
-//        } else {
-//            right = mid - 1
 //        }
 //    }
-//    return -1
-//}
+//    return nums[left]
+    
+func findMin(_ nums: [Int]) -> Int {
+            //corner cases
+    if nums == nil || nums.count == 0 {
+        return -1
+    }
+    if nums.count == 1 {
+        return nums[0]
+    }
+    
+    
+    var left = 0
+    var right = nums.count - 1
+    if nums[right] > nums[left] {
+        return nums[0]
+    }
+    while left <= right {
+       var mid = left + (right - left) / 2
+    //if mid > next element, mid+1 is the smallest
+        if nums[mid] > nums[mid + 1] {
+            return nums[mid + 1]
+        }
+        if nums[mid - 1] > nums[mid] {
+            return nums[mid]
+        }
+        
+        if nums[mid] > nums[left] {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+        
+    }
+    return -1
+}
 }
 class Q153_Find_Minimum_in_Rotated_Sorted_Array: XCTestCase {
 
