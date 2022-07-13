fc476f1dc5d358e8b8099f1ac3a22c70d3d31327
onebroccoli <three.broccoli@gmail.com>
2022-07-12 23:16:05 -0700

    Update Q278_First_Bad_Version.swift

--- AlgorithmSwiftTests/Q278_First_Bad_Version.swift
+++ AlgorithmSwiftTests/Q278_First_Bad_Version.swift
@@ -36,19 +36,19 @@
 import XCTest
 
 //private class Solution{
-//    func firstBadVersion(_ n: Int) -> Int {
-//            var left = 1
-//            var right = n
-//            while (left < right){
-//                let mid = left + (right - left) / 2
-//                if (isBadVersion(mid)){
-//                    right = mid
-//                } else {
-//                    left = mid + 1
-//                }
-//            }
-//            return left
+//func firstBadVersion(_ n: Int) -> Int {
+//    var left = 1
+//    var right = n
+//    while (left < right){
+//        let mid = left + (right - left) / 2
+//        if (isBadVersion(mid)){
+//            right = mid
+//        } else {
+//            left = mid + 1
 //        }
+//    }
+//    return left
+//}
 //}
 
 class Q278_First_Bad_Version: XCTestCase {
