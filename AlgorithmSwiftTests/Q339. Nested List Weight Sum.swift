945c8ded2d5d693069fd356df6d7f7b51cfc5711
onebroccoli <three.broccoli@gmail.com>
2022-07-17 23:09:16 -0700

    Update Q339. Nested List Weight Sum.swift

--- AlgorithmSwiftTests/Q339. Nested List Weight Sum.swift
+++ AlgorithmSwiftTests/Q339. Nested List Weight Sum.swift
@@ -39,71 +39,89 @@
  The values of the integers in the nested list is in the range [-100, 100].
  The maximum depth of any integer is less than or equal to 50.
  */
-//import XCTest
-//
-//class NestedInteger {
-//    // Return true if this NestedInteger holds a single integer, rather than a nested list.
-//    public func isInteger() -> Bool
-//
-//    // Return the single integer that this NestedInteger holds, if it holds a single integer
-//    // The result is undefined if this NestedInteger holds a nested list
-//    public func getInteger() -> Int
-//
-//    // Set this NestedInteger to hold a single integer.
-//    public func setInteger(value: Int)
-//
-//    // Set this NestedInteger to hold a nested list and adds a nested integer to it.
-//    public func add(elem: NestedInteger)
-//
-//    // Return the nested list that this NestedInteger holds, if it holds a nested list
-//    // The result is undefined if this NestedInteger holds a single integer
-//    public func getList() -> [NestedInteger]
-//}
-//
-//private class Solution {
-//    func depthSum(_ nestedList: [NestedInteger]) -> Int {
-//        return dfs(nestedList, 1)
-//    }
-//
-//    func dfs(_ list: [NestedInteger], _ depth: Int) -> Int {
-//        if list.count == 0 {
-//            return 0
-//        }
-//        var total = 0
-//        for item in list {
-//            if item.isInteger() { //if item is integer
-//                total += item.getInteger() * depth
-//            } else { //if item is a nested list
-//                total += dfs(item.getList(), depth + 1)
-//
-//            }
-//        }
-//        return total
-//    }
-//}
-//class Q339__Nested_List_Weight_Sum: XCTestCase {
-//
-//    override func setUpWithError() throws {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
-//
-//    override func tearDownWithError() throws {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
-//
-//    func testExample() throws {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//        // Any test you write for XCTest can be annotated as throws and async.
-//        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-//        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-//    }
-//
-//    func testPerformanceExample() throws {
-//        // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
-//
-//}
+import XCTest
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public func isInteger() -> Bool
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     public func getInteger() -> Int
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public func setInteger(value: Int)
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public func add(elem: NestedInteger)
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     public func getList() -> [NestedInteger]
+ * }
+ */
+
+/*
+input is nested. recursive
+go through the list one by one, keeping track of the current depth d.
+if a nested integer is an integer n, calculate its sum as n * d
+if nested integer is a list, calculate the sum of this list recursively using the same process but with depth = d + 1
+
+*/
+
+
+/*
+private class Solution {
+
+    
+    func depthSum(_ nestedList: [NestedInteger]) -> Int {
+        return dfs(nestedList, 1)
+    }
+    
+    func dfs(_ list: [NestedInteger], _ depth: Int) -> Int {
+        if list.count == 0 {
+            return 0
+        }
+        var total = 0
+        for item in list {
+            if item.isInteger() { //if item is integer
+                total += item.getInteger() * depth
+            } else { //if item is a nested list
+                total += dfs(item.getList(), depth + 1)
+                
+            }
+        }
+        return total
+    }
+}
+ 
+ */
+class Q339__Nested_List_Weight_Sum: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
